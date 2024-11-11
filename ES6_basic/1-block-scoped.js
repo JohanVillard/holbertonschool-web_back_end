05e733c65b9397aece42bdf9686fbfc9bad728bf
JohanVillard <styx.locke@gmail.com>
2024-11-11 12:03:56 +0100

    Change the name of variable in taskBlock

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
+  var task3 = false;
+  var task4 = true;
+
   if (trueOrFalse) {
     var task = true;
     var task2 = false;
   }
 
-  var task = false;
-  var task2 = true;
-
-  return [task, task2];
+  return [task3, task4];
 }
