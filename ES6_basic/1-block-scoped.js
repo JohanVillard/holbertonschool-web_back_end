e4d95398ae4b20b388a3f6ac3ee177b10272e4d2
JohanVillard <styx.locke@gmail.com>
2024-11-11 12:08:02 +0100

    ReChange order of variable decarations

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  var task3 = false;
-  var task4 = true;
-
+  
   if (trueOrFalse) {
     var task = true;
     var task2 = false;
   }
+  
+var task3 = false;
+  var task4 = true;
 
   return [task3, task4];
 }
