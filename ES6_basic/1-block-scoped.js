4e6b7b85848f076da728029fdf9ecb56da4b8d86
JohanVillard <styx.locke@gmail.com>
2024-11-11 12:06:32 +0100

    Declare with var in conditional block

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   var task4 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    var task = true;
+    var task2 = false;
   }
 
   return [task3, task4];
