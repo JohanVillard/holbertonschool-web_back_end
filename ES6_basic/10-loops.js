cabb592fcb8d3d9737986fc2224e119bb63485e2
JohanVillard <styx.locke@gmail.com>
2024-11-11 14:48:16 +0100

    Use indexOf

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const arrayCopy = [...array];
-  for (const [index, value] of arrayCopy.entries()) {
-    arrayCopy[index] = appendString + value;
+  for (const value of arrayCopy) {
+    arrayCopy[arrayCopy.indexOf(value)] = appendString + value;
   }
 
   return arrayCopy;
