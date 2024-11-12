bd808f6654d16854cb6807fe29419d0920ac112a
JohanVillard <styx.locke@gmail.com>
2024-11-12 17:37:15 +0100

    Add method check in constructor

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,6 +1,10 @@
 export default class Building {
   constructor(sqft) {
     this.sqft = sqft;
+
+    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
