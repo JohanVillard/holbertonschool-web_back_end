b2b585ff2419bf3f899841ad0a67fb6ed1814456
JohanVillard <styx.locke@gmail.com>
2024-11-12 09:40:57 +0100

    Add P1T0 to T3

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,44 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new Error('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents) || !this.isStringArray(newStudents)) {
+      throw new Error('NewStudents must be an array of strings');
+    }
+    this._students = newStudents;
+  }
+
+  static isStringArray(arr) {
+    return arr.every((i) => typeof i === 'string');
+  }
+}
