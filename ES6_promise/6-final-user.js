658bdc6422f444b39625368828c97affb4504aa5
JohanVillard <styx.locke@gmail.com>
2024-11-18 21:42:29 +0100

    Add all P1's tasks

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+
+  return Promise.allSettled([user, photo])
+    .then((data) => data.map((result) => ({
+      status: result.status,
+      value: result.status === 'fullfilled' ? result.value : result.reason,
+    })));
+}
