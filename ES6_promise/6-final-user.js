0fd676097bea905b894068cd057c0f3bc60b8ce9
JohanVillard <styx.locke@gmail.com>
2024-11-19 18:24:58 +0100

    Transform into async function

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,11 +1,11 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
 
   const promises = [user, photo];
 
-  return Promise.allSettled(promises).then((results) => results);
+  return await Promise.allSettled(promises).then((results) => results);
 }
