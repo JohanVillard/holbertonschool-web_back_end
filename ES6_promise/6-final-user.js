d26988f822d58acf0154c223b21fa7023f4bab62
JohanVillard <styx.locke@gmail.com>
2024-11-19 18:26:35 +0100

    Change function

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,10 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const user = signUpUser(firstName, lastName);
-  const photo = uploadPhoto(fileName);
-
-  const promises = [user, photo];
-
-  return await Promise.allSettled(promises).then((results) => results);
+  const result = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return result.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : `Error: ${result.reason.message}`,
+  }));
 }
