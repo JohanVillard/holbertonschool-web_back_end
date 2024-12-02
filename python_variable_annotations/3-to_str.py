85e56d647973b247313b2600775d0612f79b447a
JohanVillard <styx.locke@gmail.com>
2024-12-02 16:50:23 +0100

    Fix docstring: integer -> float

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """This module defines a to_str function."""
 
 
 def to_str(n: float) -> str:
     """
-    Convert a integer into a string.
+    Convert a float into a string.
 
     Args:
-        n (int): The integer to convert.
+        n (int): The float to convert.
 
     Returns:
         str: The converted string.
