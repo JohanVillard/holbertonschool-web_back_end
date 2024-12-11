7567f2ef6463b587307d4ab58aba62dcebfb804b
JohanVillard <styx.locke@gmail.com>
2024-12-11 14:47:33 +0100

    Test

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -83,6 +83,6 @@ class Server:
         return {
             "index": index,
             "data": data,
-            "page_size": len(data),
+            "page_size": page_size,
             "next_index": next_index,
         }
