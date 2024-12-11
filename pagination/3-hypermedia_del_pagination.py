e4bfaf8102627dceec5d523e287bcedfe99fd486
JohanVillard <styx.locke@gmail.com>
2024-12-11 14:15:25 +0100

    Keep original typing in function

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -41,9 +41,9 @@ class Server:
 
     def get_hyper_index(
             self,
-            index: Union[int, None] = None,
+            index: int = None,
             page_size: int = 10
-    ) -> Dict[str, Union[int, List[List]]]:
+    ) -> Dict:
         # Convert dataset into CSV
         csv = self.indexed_dataset()
 
