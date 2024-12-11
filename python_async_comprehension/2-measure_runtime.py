66f09b6448b5aa68631dd9e0bf565a00acf4c4ab
JohanVillard <styx.locke@gmail.com>
2024-12-11 11:14:11 +0100

    Run only ine async_comprehension()

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -19,12 +19,7 @@ async def measure_runtime() -> float:
     start_time = time.time()
 
     # Execute the coroutines concurrently
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await asyncio.gather(async_comprehension())
 
     runtime = time.time() - start_time
 
