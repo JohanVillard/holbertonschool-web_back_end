72c95b52915645e3ed915d5b657c9aef04b6b527
JohanVillard <styx.locke@gmail.com>
2024-12-11 10:52:13 +0100

    Fix Pycodestyle

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,14 +8,14 @@ from typing import AsyncGenerator
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """
-    Asynchronously generate random numbers. 
+    Asynchronously generate random numbers.
 
     This coroutine loops 10 times, waiting 1 second between iterations,
-    and yields a random float between 0 and 10. 
+    and yields a random float between 0 and 10.
 
     Returns:
        AsyncGenerator[float, None]: An asynchronous generator yielding
-       ten random numbers. 
+       ten random numbers.
     """
     for i in range(10):
         await asyncio.sleep(1)
