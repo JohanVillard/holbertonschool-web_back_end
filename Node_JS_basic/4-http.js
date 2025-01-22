e03575aa16f17f5c1561d4314f0c3d7f1df57e9e
JohanVillard <styx.locke@gmail.com>
2025-01-22 12:35:29 +0100

    Fix: change typo server to app

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,10 +1,10 @@
-const { createServer } = require('node:http');
+const { createServer } = require("node:http");
 
 const port = 1245;
 
-const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!');
+const app = createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello Holberton School!");
 });
 
-server.listen(port);
+app.listen(port);
