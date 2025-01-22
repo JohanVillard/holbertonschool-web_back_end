2bd0d028d344c44fd7b391990147ac164aa224be
JohanVillard <styx.locke@gmail.com>
2025-01-22 10:55:15 +0100

    Lint files

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on("data", input => {
+process.stdin.on('data', (input) => {
   process.stdout.write(`Your name is ${input}`);
 
   // End the stdin process
   process.stdin.end();
 });
 
-process.on("exit", () => {
-  console.log("This important software is now closing");
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,11 +1,11 @@
-const fs = require("fs");
+const fs = require('fs');
 
 function countStudents(path) {
   try {
     // Use fs.readFileSync() for a synchronous version
-    const data = fs.readFileSync(path, "utf8");
+    const data = fs.readFileSync(path, 'utf8');
     // Split the file into lines and remove empty lines
-    const lines = data.split("\n").filter(line => line);
+    const lines = data.split('\n').filter((line) => line);
 
     // Remove the header
     const studentData = lines.slice(1);
@@ -14,14 +14,14 @@ function countStudents(path) {
     const SWE = [];
 
     // Iterate over each line to access individual rows
-    studentData.forEach(line => {
+    studentData.forEach((line) => {
       // Split each line by comma to access individual values
-      const values = line.split(",");
+      const values = line.split(',');
       const field = values.length - 1;
 
-      if (values[field] === "CS") {
+      if (values[field] === 'CS') {
         CS.push(values[0]);
-      } else if (values[field] === "SWE") {
+      } else if (values[field] === 'SWE') {
         SWE.push(values[0]);
       }
     });
@@ -29,13 +29,13 @@ function countStudents(path) {
     process.stdout.write(`Number of students: ${CS.length + SWE.length}\n`);
 
     process.stdout.write(
-      `Number of students in CS: ${CS.length}. List: ${CS.join(", ")}\n`
+      `Number of students in CS: ${CS.length}. List: ${CS.join(', ')}\n`,
     );
     process.stdout.write(
-      `Number of students in SWE: ${SWE.length}. List: ${SWE.join(", ")}\n`
+      `Number of students in SWE: ${SWE.length}. List: ${SWE.join(', ')}\n`,
     );
   } catch (error) {
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
 }
 
