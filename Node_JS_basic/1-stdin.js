e45a1a241b3a6e3b77483f51f462c74cde1c5367
JohanVillard <styx.locke@gmail.com>
2025-01-22 10:31:58 +0100

    Replace 2 process.stdout.write by console.log

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+console.log("Welcome to Holberton School, what is your name?");
 
 process.stdin.on("data", input => {
   process.stdout.write(`Your name is ${input}`);
@@ -7,5 +7,5 @@ process.stdin.on("data", input => {
 });
 
 process.on("exit", () => {
-  process.stdout.write("This important software is now closing\n");
+  console.log("This important software is now closing");
 });
