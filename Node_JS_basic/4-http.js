dabca0274c46639a3e8f0d97688509f09b5e63a0
JohanVillard <styx.locke@gmail.com>
2025-01-22 12:43:16 +0100

    Export app

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,10 +1,11 @@
-const http = require('http');
+const http = require("http");
+const { defaults } = require("request");
 
 const port = 1245;
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!');
+export default app = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello Holberton School!");
 });
 
 app.listen(port);
