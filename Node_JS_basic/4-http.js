87095d133bdd621eba61e0d640c8f60b99fd61e1
JohanVillard <styx.locke@gmail.com>
2025-01-22 12:39:43 +0100

    Import all http module

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,8 +1,8 @@
-const { createServer } = require('node:http');
+const http = require('http');
 
 const port = 1245;
 
-const app = createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 });
