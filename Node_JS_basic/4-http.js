74397d5d31d1abcc5b3d7751318f42a5ade4e95a
JohanVillard <styx.locke@gmail.com>
2025-01-22 12:36:55 +0100

    Apply linter

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,10 +1,10 @@
-const { createServer } = require("node:http");
+const { createServer } = require('node:http');
 
 const port = 1245;
 
 const app = createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello Holberton School!");
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
 });
 
 app.listen(port);
