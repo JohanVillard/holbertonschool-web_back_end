ba964ba9b281f2d15ee6964030301e1fd71ef6c3
JohanVillard <styx.locke@gmail.com>
2025-01-22 11:27:35 +0100

    Fix: typo add missing ':' after 'Your name is'

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-console.log('Welcome to Holberton School, what is your name?');
+console.log("Welcome to Holberton School, what is your name?");
 
-process.stdin.on('data', (input) => {
+process.stdin.on("data", input => {
   const name = input.toString().trim();
-  process.stdout.write(`Your name is ${name}`);
+  process.stdout.write(`Your name is: ${name}`);
 
   // End the stdin process
   process.exit();
 });
 
-process.on('exit', () => {
-  console.log('This important software is now closing');
+process.on("exit", () => {
+  console.log("This important software is now closing");
 });
